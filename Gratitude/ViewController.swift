1b78e22ee72493e90ed5bc19aa7eaed79c8ef00a
Pradeep Singh <pradeepsingh@Pradeeps-MacBook-Air.local>
2017-07-26 22:16:00 +0530

    Initial Commit

---
+++ Gratitude/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gratitude
+//
+//  Created by Pradeep Singh on 26/07/17.
+//  Copyright © 2017 Pradeep Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
